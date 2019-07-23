6bc354fc932eda53dce112a96ed1561d187ca297
Ted John <ted@brambles.org>
2019-07-23 23:23:41 +0100

    Add code for creating custom window and demonstrating the hot reload

--- window-tools.js
+++ window-tools.js
@@ -1,3 +1,4 @@
+/// <reference path="C:/Users/Ted/Documents/Projects/OpenRCT2/GitHub/OpenRCT2/distribution/openrct2.d.ts" />
 // Sample script that adds functions for managing the player's windows
 
 var organiseWindows = function() {
@@ -29,6 +30,52 @@ var main = function() {
         title: 'Organises all opened windows',
         shortcut: 'CTRL+SHIFT+O',
         action: organiseWindows
+    });
+
+    ui.closeAllWindows();
+    var w = ui.openWindow({
+        classification: "custom.my",
+        title: "My Custom Window",
+        x: 0,
+        y: 27,
+        width: 200,
+        height: 250,
+        widgets: [
+            {
+                type: 'button',
+                x: 84,
+                y: 31,
+                width: 54,
+                height: 16,
+                text: 'Close',
+                onClick: function() {
+                    park.postMessage('Cancel this!');
+                }
+            },
+            {
+                type: 'button',
+                x: 141,
+                y: 31,
+                width: 54,
+                height: 16,
+                text: 'Apply',
+                onClick: function() {
+                    park.postMessage('Apply!');
+                }
+            },
+            {
+                type: 'dropdown',
+                x: 3,
+                y: 16,
+                width: 192,
+                height: 12,
+                items: ['a', 'b', 'c'],
+                selectedIndex: 1,
+                onChange: function(index) {
+                    park.postMessage("Changed to item #" + index);
+                }
+            }
+        ]
     });
 };
 
